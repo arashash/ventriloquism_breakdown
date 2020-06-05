054eeca60697e67128016119e1cecb92ca27cb24
arash <arash.ashrafnejad@gmail.com>
2020-06-05 23:26:48 +0300

    uncompressed sounds are generated

---
+++ sound_generator.py
@@ -0,0 +1,34 @@
+"""Python 2 and 3 sterio sound gnerator for breakdown of Ventriloquism"""
+# it runs much faster with Python 2!
+
+from struct import pack
+from math import sin, pi
+import wave
+import random
+
+SAMPLE_RATE = 44100 # in samples per second
+FREQ = 400.0 # frequency in HZ
+TIME = 8 # in seconds
+STEPS = 1000 # decay steps
+DECAY_RATE = 0.999 # decay per step
+AMPLITUDE = 2 ** 15 - 1.0 # maximum amplitude
+DECAY_DIR = 'Right' # which channel to decay
+
+length = SAMPLE_RATE * TIME
+step_lenght = length // STEPS
+leftMaxVol = rightMaxVol = AMPLITUDE
+
+wv = wave.open('%s_decay.mp3'%DECAY_DIR, 'w')
+wv.setparams((2, 2, SAMPLE_RATE, 0, 'NONE', 'not compressed'))
+wvData = b''
+for i in range(length):
+    wvData += pack('h', int(rightMaxVol * sin(i * FREQ / SAMPLE_RATE))) # right
+    wvData += pack('h', int(leftMaxVol * sin(i * FREQ / SAMPLE_RATE))) # left
+    if i % step_lenght == 0:
+        if DECAY_DIR == 'Left':
+            leftMaxVol *= DECAY_RATE
+        else:
+            rightMaxVol *= DECAY_RATE
+
+wv.writeframes(wvData)
+wv.close()
