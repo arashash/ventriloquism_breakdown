fb1cd055366db44d806143a862efda56a68bac3e
arash <arash.ashrafnejad@gmail.com>
2020-06-06 02:23:22 +0300

    codes refactored a bit, timer added, sounds compressed in audacity

---
+++ main.js
@@ -0,0 +1,39 @@
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('toggle');
+var resetBtn = document.getElementById('reset');
+var watch = new Stopwatch(timer);
+
+
+var beeeep = new Audio();
+var state = 'LEFT'
+var timer = 0
+function start(num){
+  if ((Math.random() > 0.5)) {
+    state = 'RIGHT'
+    beeeep.src = 'Right_decay.mp3';
+  } else {
+    state = 'LEFT'
+    beeeep.src = 'Left_decay.mp3';
+  }
+  watch.reset();
+  beeeep.play();
+  watch.start();
+}
+
+function left(){
+  watch.stop();
+  if (state == 'LEFT') {
+    div1.innerHTML = "CORRECT";
+  } else {
+    div1.innerHTML = "WRONG";
+  }
+}
+
+function right(){
+  watch.stop();
+  if (state == 'RIGHT') {
+    div1.innerHTML = "CORRECT";
+  } else {
+    div1.innerHTML = "WRONG";
+  }
+}
